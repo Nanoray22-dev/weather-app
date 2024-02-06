483dda515dda581d70c3ec01ab7d2e3686485fec
Raysell Concepcion <raysellconcepcionpaulino@gmail.com>
2024-02-05 22:24:47 -0400

    adding to export file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
+import { useContext } from 'react'
 import './App.css'
+import { UserContext } from './Components/Context/userContext'
+import Search from './Components/Search'
+import MainDashboard from './Components/MainDashboard'
+import SubDashboard from './Components/SubDashboard'
 
 function App() {
-
+const {isSearch} = useContext(UserContext)
 
   return (
-    <>
-      
-    </>
+    <section className='App'>
+      {
+        isSearch ? <Search/> : <MainDashboard/>
+      }
+      <SubDashboard/>
+    </section>
   )
 }
 
